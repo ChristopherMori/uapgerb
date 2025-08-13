1465be155609a8ef14496d00d1fce56fa72019e0
Christopher Mori <enisity@gmail.com>
2025-08-13 11:57:07 -0400

    Properly escape quotes in page builder

--- scripts/06_build_pages.py
+++ scripts/06_build_pages.py
@@ -196,7 +196,7 @@ def build_video_pages(videos: List[Video]) -> Dict[str, List[Tuple[str, str]]]:
         fm = [
             "---",
             f"id: {v.id}",
-            f'title: "{v.title.replace('"', '\"')}",'
+            f'title: "{v.title.replace("\"", "\\\"")}",'
             f"published_at: {v.published_at or ''}",
             f"duration: {v.duration or ''}",
             f"tags: {v.tags!r}",
